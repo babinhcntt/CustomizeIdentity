ee3670329315219bd17efce0609bcac09b670472
nbbinh <nbbinh@cmc.com.vn>
2020-05-04 12:47:30 +0700

    test

---
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    //IdentityDbContext<User, Role, string, UserClaim, UserRole, UserLogin, RoleClaim, UserToken>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //modelBuilder.Entity<User>(b =>
+            //{
+            //    // Each User can have many UserClaims
+            //    b.HasMany(e => e.Claims)
+            //        .WithOne(e => e.User)
+            //        .HasForeignKey(uc => uc.UserId)
+            //        .IsRequired();
+
+            //    // Each User can have many UserLogins
+            //    b.HasMany(e => e.Logins)
+            //        .WithOne(e => e.User)
+            //        .HasForeignKey(ul => ul.UserId)
+            //        .IsRequired();
+
+            //    // Each User can have many UserTokens
+            //    b.HasMany(e => e.Tokens)
+            //        .WithOne(e => e.User)
+            //        .HasForeignKey(ut => ut.UserId)
+            //        .IsRequired();
+
+            //    // Each User can have many entries in the UserRole join table
+            //    b.HasMany(e => e.UserRoles)
+            //        .WithOne(e => e.User)
+            //        .HasForeignKey(ur => ur.UserId)
+            //        .IsRequired();
+            //});
+
+            //modelBuilder.Entity<Role>(b =>
+            //{
+            //    // Each Role can have many entries in the UserRole join table
+            //    b.HasMany(e => e.UserRoles)
+            //        .WithOne(e => e.Role)
+            //        .HasForeignKey(ur => ur.RoleId)
+            //        .IsRequired();
+
+            //    // Each Role can have many associated RoleClaims
+            //    b.HasMany(e => e.RoleClaims)
+            //        .WithOne(e => e.Role)
+            //        .HasForeignKey(rc => rc.RoleId)
+            //        .IsRequired();
+            //});
+            //modelBuilder.Entity<UserLogin>(b =>
+            //{
+            //    b.HasNoKey();
+            //   // b.HasKey(l => new { l.LoginProvider, l.ProviderKey });
+            //});
+            //Table name
+            modelBuilder.Entity<IdentityUser>(b =>
+            {
+                b.ToTable("Users");
+            });
+
+            modelBuilder.Entity<IdentityUserClaim<string>>(b =>
+            {
+                b.ToTable("UserClaims");
+            });
+
+            modelBuilder.Entity<IdentityUserLogin<string>>(b =>
+            {
+                b.ToTable("UserLogins");
+            });
+
+            modelBuilder.Entity<IdentityUserToken<string>>(b =>
+            {
+                b.ToTable("UserTokens");
+            });
+
+            modelBuilder.Entity<IdentityRole>(b =>
+            {
+                b.ToTable("Roles");
+            });
+
+            modelBuilder.Entity<IdentityRoleClaim<string>>(b =>
+            {
+                b.ToTable("RoleClaims");
+            });
+
+            modelBuilder.Entity<IdentityUserRole<string>>(b =>
+            {
+                b.ToTable("UserRoles");
+            });
+        }
+    }
+}
---
+++ WebApplication1/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class User : IdentityUser
+    {
+        public virtual ICollection<UserClaim> Claims { get; set; }
+        public virtual ICollection<UserLogin> Logins { get; set; }
+        public virtual ICollection<UserToken> Tokens { get; set; }
+        public virtual ICollection<UserRole> UserRoles { get; set; }
+    }
+}
